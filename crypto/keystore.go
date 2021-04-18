99ecc984a7938d70e423283fb28ae2a7fbe3c11b
aditya.k <adityak368@gmail.com>
2021-04-18 10:06:26 +0200

    Added key addition from string

--- crypto/keystore.go
+++ crypto/keystore.go
@@ -19,6 +19,16 @@ func (store *KeyStore) AddKeyFromFile(keyName, fileName string) error {
 	return nil
 }
 
+// AddKey Adds a private rsa key to the keystore
+func (store *KeyStore) AddKey(keyName, pem string) error {
+	key, err := ParseRsaPrivateKeyFromPemStr(pem)
+	if err != nil {
+		return err
+	}
+	store.keys[keyName] = key
+	return nil
+}
+
 // Key Gets the private key for the given identifier
 func (store *KeyStore) Key(keyName string) (*rsa.PrivateKey, bool) {
 	val, isOk := store.keys[keyName]
